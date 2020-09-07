68d5ebc3d073918053a09e609b71a029a36c95b1
Massau <joyce.santos202@etec.sp.gov.br>
2020-09-07 17:20:48 -0300

    Cria tela de simulação sete

---
+++ view/iniciarSimulacaoSete.php
@@ -0,0 +1,30 @@
+<?php include('../layouts/head.php') ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="container-fluid">
+<?php include('../layouts/navbar.php') ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card my-5">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Fim da simulação do procedimento de formalização</h5>            
+                                                   
+                        <div class="col-md-10 mx-auto">                           
+                            <p class="pt-4 ml-4 text-info">Parabéns, agora você tem cidadania empresarial, você é um(a) Microempreendedor Individual - MEI.</p>
+                            <p class="ml-4">Continue seguindo os passos procedimentos e simule o ambiente de sua empresa recém criada.</p>
+                        </div>
+                                            
+                    </div>
+                    <div class="mb-4 btn-group mx-auto" role="group">
+                        <a href="iniciarSimulacaoUm.php" class="btn btn-lg btn-secondary">Voltar</a>
+                        <a href="" class="btn btn-lg btn-secondary">Pular introdução ao MEI</a>
+                        <a href="iniciarSimulacaoSete.php" class="btn btn-lg btn-primary disable">Continuar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('../layouts/footer.php') ?>
+
