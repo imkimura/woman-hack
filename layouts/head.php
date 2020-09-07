ee1f4957105648dc6e473db6c9e0483c275fe234
Massau <joyce.santos202@etec.sp.gov.br>
2020-09-07 09:56:01 -0300

    Cria readMe e altera estrutura de diretórios

--- layouts/head.php
+++ layouts/head.php
@@ -6,7 +6,7 @@
     <title>Woman Hack</title>
     <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;700&family=Poppins&display=swap" rel="stylesheet">    
 
-    <link rel="stylesheet" href="./src/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./src/css/styles.css">
+    <link rel="stylesheet" href="../src/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../src/css/styles.css">
 </head>
 <body>
---
+++ view/index.php
@@ -0,0 +1,13 @@
+<?php 
+    include('../layouts/head.php');
+    include('../layouts/navbar.php');
+?>
+
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+
+<?php include('../layouts/footer.php') ?>
+ 
