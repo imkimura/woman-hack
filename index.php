c44181b7c65754e82079e37a43bc6268568b8c36
Massau <joyce.santos202@etec.sp.gov.br>
2020-09-07 09:05:12 -0300

    Move navbar para outro arquivo e realiza include

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
-<?php include('layouts/head.php') ?>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">Logo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+<?php 
+    include('layouts/head.php');
+    include('layouts/navbar.php');
+?>
 
     <div class="container">
         <div class="row">
